53b181fd15499df8df48896eb6d9d4ce1aa8e12c
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-11-08 23:50:06 +0600

    добавил класс ResultArray.cs для вывода результатов

---
+++ TestAssignment/ResultClasses/ResultArray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using TestAssignment.Interface;
+
+namespace TestAssignment.ResultClasses
+{
+    public class ResultArray
+    {
+        public string [] Results(int number)
+        {
+            List<IResult> fooBarObjects = new List<IResult>();
+            fooBarObjects.Add(new ResultIfTwo());
+            fooBarObjects.Add(new ResultIfThree());
+            fooBarObjects.Add(new ResultIfFour());
+            fooBarObjects.Add(new ResultIfFive());
+            fooBarObjects.Add(new ResultIfSix());
+            fooBarObjects.Add(new ResultIfSeven());
+            fooBarObjects.Add(new ResultIfEight());
+            fooBarObjects.Add(new ResultIfNine());
+            string [] array = new string[fooBarObjects.Count];
+
+            for (int i = 0; i < fooBarObjects.Count; i++)
+            {
+                array[i] = fooBarObjects[i].Result(number);
+            }
+            return array;
+        }
+    }
+}
