a1ca7f626830772ff3c4a62d0d16e5093247f2d3
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-11-08 23:48:57 +0600

    добавил контроллер NumberController.cs с одним методом

---
+++ TestAssignment/Controllers/NumberController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TestAssignment.ResultClasses;
+
+
+namespace TestAssignment.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NumberController : Controller
+    {
+        private readonly ResultArray _resultArray;
+
+        public NumberController(ResultArray resultArray)
+        {
+            _resultArray = resultArray;
+        }
+        
+        [HttpGet("{num:int}")]
+        public ActionResult<string> Get(int num)
+        {
+            return Ok(String.Join(String.Empty, _resultArray.Results(num)));
+        }
+    }
+}
